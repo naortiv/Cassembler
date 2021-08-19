3872d0b784a2c1db62a31154b80c0ed59a4292b4
shiramar <86928995+shiramar@users.noreply.github.com>
2021-08-19 16:21:09 +0300

    Update struct.h

--- struct.h
+++ struct.h
@@ -4,32 +4,32 @@
 
 #include "assembler.h"
 
-/* Arrays for storing the data image and instructions image */
+/* data image and instructions image arrays */
 extern unsigned char data_image[];
 extern unsigned int instructions_image[];
 
-/* Declaring a boolian variable */
+/* boolean variable */
 typedef enum { FALSE, TRUE } boolean;
 
-/* Creating a linked list for a label table */
+/* we creat linked list for a label table */
 typedef struct structLabels* labelPtr;
 typedef struct structLabels {
 	char name[MAX_LABEL];/* The name of the label */
 	unsigned int address;/* The address of the label */
 	char attributes[MAX_LABEL];/* The symbol type of the label */
-	boolean external;/* A boolean type variable to store if the label is extern or not */
-	boolean command_line;/* A boolean type varialbe to store if the label is in an action statement or not */
-	boolean entry;/* A boolean type varialbe to store if the label is entry or not */
-	labelPtr next;/* A pointer to the next label in the list */
+	boolean external;/* sign if the label is extern */
+	boolean command_line;/* sign if it's comannd label */
+	boolean entry;/* sign if't entry label */
+	labelPtr next;/* a pointer to the next label in the link list */
 } Labels;
 
 /*Creating a two-way linked list for extern labels */
 typedef struct ext* extPtr;
 typedef struct ext {
 	char name[MAX_LABEL]; /* The name of the extern label */
-	unsigned int address; /* The address in memory where the external address should be replaced */
-	extPtr next; /* A pointer to the next extern in the list */
-	extPtr prev; /* A pointer to the previous extern in the list */
+	unsigned int address; /* The memory address for external address */
+	extPtr next; /* A pointer to the next node extern in list */
+	extPtr prev; /* A pointer to the previous node extern in list */
 } ext;
 
 #endif
